f51730885ac93860d68fa7d406cd6441254880a0
Roberto Arcomano <bertolinux@gmai.com>
2026-04-04 23:43:26 +0200

    First commit

---
+++ frontend/dev/src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import { Button } from '@mui/material'
+import './App.css'
+
+function App() {
+  const [message, setMessage] = useState('')
+
+  const handleClick = async () => {
+    const res = await fetch('http://localhost:3200/')
+    const data = await res.json()
+    setMessage(data.message)
+  }
+
+  return (
+    <>
+      <Button variant="contained" onClick={handleClick}>Click</Button>
+      {message && <p>{message}</p>}
+    </>
+  )
+}
+
+export default App
