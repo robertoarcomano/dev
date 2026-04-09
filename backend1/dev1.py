29d96478ca860550199875265aef8b1ba140e357
Roberto Arcomano <bertolinux@gmai.com>
2026-04-09 14:12:27 +0200

    Added backend1

---
+++ backend1/dev1.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app1 = FastAPI()
+app1.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])
+
+@app1.get("/")
+def home():
+    return {"message": "Hello World1 FastAPI!"}
