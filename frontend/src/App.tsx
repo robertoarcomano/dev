c2a669d80dda93e882608ae29c85f57040e09228
Roberto Arcomano <bertolinux@gmai.com>
2026-04-09 14:36:05 +0200

    test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,18 +4,24 @@ import './App.css'
 
 function App() {
   const API_URL = import.meta.env.VITE_API_URL
+  const API1_URL = import.meta.env.VITE_API1_URL
   const [message, setMessage] = useState('')
+  const [message1, setMessage1] = useState('')
 
   const handleClick = async () => {
     const res = await fetch(API_URL)
     const data = await res.json()
     setMessage(data.message)
+    
+    const res1 = await fetch(API1_URL)
+    const data1 = await res1.json()
+    setMessage1(data1.message)
   }
 
   return (
     <>
       <Button variant="contained" onClick={handleClick}>Click</Button>
-      {message && <p>{message}</p>}
+      {message && <p>{message} {message1}</p>}
     </>
   )
 }
